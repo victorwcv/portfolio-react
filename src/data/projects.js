a577e1d7d0c37b2fac18494b89b7ce5477a52bc5
William <victorwccv@gmail.com>
2024-06-24 23:30:02 -0500

    feat: add Banking App section

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,6 +1,24 @@
 const projects = [
   {
     id: 1,
+    title: "FullStack Banking App",
+    description:
+      "This banking application, developed as my MIT professional certification capstone, boasts comprehensive functionalities tailored for both users and administrators. It features fully secure and private routes, adhering to the latest best practices in modern development for seamless implementation. Powered by the advanced MERN stack (MongoDB, Express.js, React, Node.js) and structured with a robust MVC architecture, this app ensures secure and efficient banking experiences. Explore cutting-edge features designed with user-centricity and administrative efficiency in mind.",
+    imgUrl: ["/under-construction.webp", "/under-construction.webp", "/under-construction.webp"],
+    alt: "Bank app Project Image",
+    githubLink: "https://github.com/victorwcv/MIT-capstone-bank-app",
+    shields: [
+      "https://img.shields.io/badge/React-18-blue",
+      "https://img.shields.io/badge/Redux_Toolkit-2-purple",
+      "https://img.shields.io/badge/Tailwind_CSS-3-blueviolet",
+      "https://img.shields.io/badge/Node.js-20-green",
+      "https://img.shields.io/badge/MongoDB-5-green",
+      "https://img.shields.io/badge/Vite-5-brightgreen",
+      "https://img.shields.io/badge/JWT-9-orange",
+    ]
+  },
+  {
+    id: 2,
     title: "User Authentication Project",
     description:
       "Built with the MERN stack (MongoDB, Express, React, and Node.js), my application provides a robust and scalable authentication experience, with carefully crafted styles using Tailwind CSS for an attractive and consistent appearance",
@@ -20,7 +38,7 @@ const projects = [
     ]
   },
   {
-    id: 2,
+    id: 3,
     title: "Weather Web App",
     description:
       "This is a responsive web application created with vanilla JavaScript that allows you to search for the current weather of any city in the world. It utilizes the free OpenWeatherMap API to retrieve real-time weather data",
